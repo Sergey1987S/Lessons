8f3aee0d8d7dfbc47ac7dd04efa037f8a63cb55a
Sergey1987S <125464856+Sergey1987S@users.noreply.github.com>
2023-02-21 16:57:56 +0300

    Task1.java
    Task1

---
+++ src/Task1.java
@@ -0,0 +1,61 @@
+public class Task1 {
+
+    public static void main(String[] args) {
+
+        byte number1 = 1;
+        short number2 = 2;
+        int number3 = 3;
+        long number4 = 4;
+
+        float number5 = 5.1f;
+        double number6 = 6.12;
+
+        char symbol = 'W';
+
+        boolean results = false;
+
+        System.out.println(number1);
+        System.out.println(number2);
+        System.out.println(number3);
+        System.out.println(number4);
+        System.out.println(number5);
+        System.out.println(number6);
+        System.out.println(symbol);
+        System.out.println(results);
+
+        int number7 = number2 + number3;
+        int number8 = number2 - number1;
+        double number9 = number4 / number2;
+        double number10 = number5 % number4;
+        int number11 = ++number3;
+        long number12 = number4++;
+        long number13 = --number4;
+        float number14  = number5--;
+        double number15 = number6 + number5;
+        double number16 = number6 - number5;
+        double number17 = number6 / number5;
+        double number18 = number6 % number5;
+        double number19 = ++number6;
+        double number20 = number6++;
+        double number21 = --number6;
+        double number22 = number6--;
+
+        System.out.println(number7);
+        System.out.println(number8);
+        System.out.println(number9);
+        System.out.println(number10);
+        System.out.println(number11);
+        System.out.println(number12);
+        System.out.println(number13);
+        System.out.println(number14);
+        System.out.println(number15);
+        System.out.println(number16);
+        System.out.println(number17);
+        System.out.println(number18);
+        System.out.println(number19);
+        System.out.println(number20);
+        System.out.println(number21);
+        System.out.println(number22);
+
+    }
+}
